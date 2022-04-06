25b3cc22ea710c5ac9830a154b3db57540ca54d8
dariomtz <gusdamtzr@gmail.com>
2022-04-05 19:58:39 -0500

    Init

---
+++ lib/main.dart
@@ -0,0 +1,60 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:foto_share/auth/bloc/auth_bloc.dart';
+import 'package:foto_share/content/espera/bloc/pending_bloc.dart';
+import 'package:foto_share/home/home_page.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import 'login/login_page.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(
+    MultiBlocProvider(
+      providers: [
+        BlocProvider(
+          create: (context) => AuthBloc()..add(VerifyAuthEvent()),
+        ),
+        BlocProvider(
+          create: (context) => PendingBloc()..add(GetAllMyDisabledFotosEvent()),
+        ),
+      ],
+      child: MyApp(),
+    ),
+  );
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: Colors.purple,
+        ),
+      ),
+      home: BlocConsumer<AuthBloc, AuthState>(
+        listener: (context, state) {
+          if (state is AuthErrorState) {
+            ScaffoldMessenger.of(context).showSnackBar(
+              SnackBar(
+                content: Text("Favor de autenticarse"),
+              ),
+            );
+          }
+        },
+        builder: (context, state) {
+          if (state is AuthSuccessState) {
+            return HomePage();
+          } else if (state is UnAuthState ||
+              state is AuthErrorState ||
+              state is SignOutSuccessState) {
+            return LoginPage();
+          }
+          return Center(child: CircularProgressIndicator());
+        },
+      ),
+    );
+  }
+}
